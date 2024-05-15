717147dbf442469b1b8cc0fd4921c68fce71b997
Myriam <160158729+myrmlbst@users.noreply.github.com>
2024-05-15 15:41:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,19 @@
 #include <iostream>
 
 constexpr std::size_t
-get_cell(std::size_t row, std::size_t col) noexcept { return (row/3) * 3+col / 3; }
+get_cell(std::size_t row, std::size_t col) noexcept { 
+    return ((row / 3) * (3 + col)) / 3; 
+}
 
 constexpr std::size_t
-get_next_row(std::size_t row, std::size_t col) noexcept { return row+(col+1) / 9; }
+get_next_row(std::size_t row, std::size_t col) noexcept { 
+    return (row + (col + 1)) / 9; 
+}
 
 constexpr std::size_t
-get_next_col(std::size_t col) noexcept { return (col+1) % 9; }
+get_next_col(std::size_t col) noexcept { 
+    return (col + 1) % 9; 
+}
 
 
 class Solution {
@@ -23,6 +29,7 @@ public:
         for (std::size_t row=0; row<9; ++row) {
             for (std::size_t col=0; col<9; ++col) {
                 char digit;
+                
                 if ((digit = board[row][col]) != '.') {
                     std::size_t digit_idx = digit - '1';
                     row_contains[row].set(digit_idx);
@@ -39,7 +46,9 @@ private:
     static constexpr std::pair<std::size_t, std::size_t>
     next_empty_position(std::vector<std::vector<char>> &board, std::size_t row, std::size_t col) noexcept {
         while (row != 9) {
-            if (board[row][col] == '.') { return {row, col}; }
+            if (board[row][col] == '.') { 
+                return {row, col}; 
+            }
             row = get_next_row(row, col);
             col = get_next_col(col);
         }
@@ -50,15 +59,19 @@ private:
     solve(std::vector<std::vector<char>> &board, std::size_t const row_start, std::size_t const col_start,
           std::array<std::bitset<9>, 9> &row_contains,
           std::array<std::bitset<9>, 9> &col_contains,
-          std::array<std::bitset<9>, 9> &cell_contains) noexcept
-    {
+          std::array<std::bitset<9>, 9> &cell_contains) noexcept {
         auto [row, col] = next_empty_position(board, row_start, col_start);
 
-        if (row == 9) { return true; }
+        if (row == 9) { 
+            return true; 
+        }
 
         std::size_t const cell = get_cell(row, col);
         std::bitset<9> const contains = row_contains[row] | col_contains[col] | cell_contains[cell];
-        if (contains.all()) { return false; }
+        
+        if (contains.all()) { 
+            return false; 
+        }
 
         for (std::size_t digit_idx=0; digit_idx<9; ++digit_idx) {
             if (!contains[digit_idx]){
@@ -67,7 +80,9 @@ private:
                 col_contains[col].set(digit_idx);
                 cell_contains[cell].set(digit_idx);
                 
-                if (solve(board, row, col, row_contains, col_contains, cell_contains)) { return true; }
+                if (solve(board, row, col, row_contains, col_contains, cell_contains)) { 
+                    return true; 
+                }
 
                 row_contains[row].reset(digit_idx);
                 col_contains[col].reset(digit_idx);
@@ -81,8 +96,7 @@ private:
 };
 
 
-void print_board(std::vector<std::vector<char>> board)
-{
+void print_board(std::vector<std::vector<char>> board) {
     std::cout << "\t\t";
     for (std::size_t row=0; row<9; ++row) {
         for (std::size_t col=0; col<9; ++col) {
@@ -95,6 +109,7 @@ void print_board(std::vector<std::vector<char>> board)
 std::vector<std::vector<char>> flat_board_to_vec_vec(std::array<char, 81> const flat_board) {
     std::vector<std::vector<char>> board;
     board.reserve(9);
+    
     for (std::size_t row=0; row<9; ++row) {
         std::vector<char> this_row;
         this_row.reserve(9);
@@ -108,6 +123,7 @@ std::vector<std::vector<char>> flat_board_to_vec_vec(std::array<char, 81> const 
 
 
 int main() {
+    // board to be solved
     std::array<char, 81> const flat_board = {
         '.', '7', '.', '.', '2', '.', '9', '.', '.',
         '.', '4', '.', '8', '.', '6', '.', '.', '.',
@@ -119,7 +135,8 @@ int main() {
         '.', '.', '.', '5', '.', '4', '.', '3', '.',
         '.', '.', '7', '.', '6', '.', '.', '1', '.'
     };
-    
+
+    // expected outcome
     std::array<char, 81> const flat_expected = {
         '8', '7', '6', '1', '2', '3', '9', '4', '5',
         '5', '4', '3', '8', '9', '6', '7', '2', '1',
@@ -144,6 +161,7 @@ int main() {
     print_board(expected);
 
     soln.solveSudoku(board);
+    
     if (board==expected) {
         std::cout << "\n\n\t\t[OPERATION SUCCESSFUL]";
     } 
